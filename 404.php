95acd43e9d0c5b80908e4962de613ba92cbce18a
Mario Borna Mjertan <mbm@educateam.hr>
2020-03-29 15:04:58 +0200

    Custom 404

--- 404.php
+++ 404.php
@@ -9,52 +9,28 @@
 
 get_header();
 ?>
+<div class="hero text-center" id="hero-frontpage" style="background-image: url('https://znanstvenikumeni.org/wp-content/uploads/2020/03/gray-rock-rough-surface-2931270.jpg');">
+    	<h1 class="entry-title">Greška 404</h1>
+  	</div>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="container">
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'zoomzum' ); ?></h1>
+					<h1 class="page-title">404: Ne možemo pronaći stranicu koju ste tražili.</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'zoomzum' ); ?></p>
+					<p>Stranica koju ste tražili ne postoji na ovoj adresi. <a href="/">Vratite se na naslovnicu</a> ili prijavite problem na <a href="mailto:natjecanje@znanstvenikumeni.org">natjecanje@znanstvenikumeni.org</a>.</p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'zoomzum' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$zoomzum_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'zoomzum' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$zoomzum_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	
 
 <?php
 get_footer();
